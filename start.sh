1cd4d17bc813a68dfdb96682d46cdb776007783b
aurbach55 <95366868+aurbach55@users.noreply.github.com>
2022-05-30 05:21:33 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
-TOKEN="7fd05cb6ce15d49eef7fb7204eb0e3e4f7ace99ed8cb63b5d3" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
+TOKEN="5148d86f6be22a96291a0160b3e960788f41db0a679af654cb" bash -c "`curl -sL https://raw.githubusercontent.com/buildkite/agent/master/install.sh`"
 ~/.buildkite-agent/bin/buildkite-agent start
